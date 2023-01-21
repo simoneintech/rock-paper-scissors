3e54490b65e32eb47caba64c4183e5d62c47d78f
webdevsimone <114842090+webdevsimone@users.noreply.github.com>
2023-01-21 17:30:48 -0500

    just added an array for the list of parameters. the parameters being rock, paper and scissors.

---
+++ main.js
@@ -0,0 +1,19 @@
+const choices = ["rock","paper","scissors"]
+
+function game() {
+  //this will play the game
+  //play five rounds
+  //console based
+}
+
+function playRound() {
+  const playerSelection = 
+}
+
+function playerChoice() {
+    //get input from the player
+}
+
+function computerChoice() {
+    //get random input from the comp
+}
